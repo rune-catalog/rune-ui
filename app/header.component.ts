8dd3e21b5dacde2bc00f3879e0c13e68423e0b06
Sam Noedel <sam.noedel@gmail.com>
2016-09-15 21:04:11 -0700

    Remove table layout

--- app/header.component.ts
+++ app/header.component.ts
@@ -3,13 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rune-header',
   template: `
-  <header>
     <h1>RUNE</h1>
-    <span class="expando"></span>
     <a><i class="icon icon-search"></i></a>
     <a><i class="icon icon-add"></i></a>
     <span class="divider"></span>
-    <a><i class="icon icon-menu"></i></a>
-  </header>`
+    <a><i class="icon icon-menu"></i></a>`
 })
 export class HeaderComponent { }
